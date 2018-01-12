f06c3aaf381dfd17a2df2b0ccc6203d9b4b24a02
sion <sion@siondeMac.local>
2018-01-12 18:36:49 +0800

    delete doc

--- src/main/java/com/github/sioncheng/esp/MainApp.java
+++ src/main/java/com/github/sioncheng/esp/MainApp.java
@@ -2,7 +2,11 @@ package com.github.sioncheng.esp;
 
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -13,6 +17,8 @@ import org.elasticsearch.common.xcontent.XContentFactory;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class MainApp {
 
@@ -24,9 +30,19 @@ public class MainApp {
         final String name = "secisland";
         final String type = "secilog";
 
-        createIndex(name, type);
-
-        insertDoc(name, type);
+        if ("createIndex".equalsIgnoreCase(args[0])) {
+            createIndex(name, type);
+        } else if ("insertDoc".equalsIgnoreCase(args[0])) {
+            insertDoc(name, type);
+        } else if ("updateDoc".equalsIgnoreCase(args[0])) {
+            updateDoc(name, type);
+        } else if ("getDoc".equalsIgnoreCase(args[0])) {
+            getDoc();
+        } else if ("deleteDoc".equalsIgnoreCase(args[0])) {
+            deleteDoc();
+        } else {
+            System.out.println("nothing to do");
+        }
 
         client.close();
     }
@@ -91,6 +107,47 @@ public class MainApp {
 
         System.out.println(String.format("index: %s insert doc id: %s", response.getIndex(), response.getId()));
     }
+
+    static void updateDoc(String indexName, String type) throws IOException, ExecutionException, InterruptedException {
+        UpdateRequest updateRequest = new UpdateRequest();
+
+        updateRequest.index(indexName);
+        updateRequest.type(type);
+        updateRequest.id("1");
+
+        XContentBuilder doc = XContentFactory.jsonBuilder().startObject()
+                .field("type", "syslog")
+                .field("eventCount", 1)
+                .field("eventDate", new Date())
+                .field("message", "secilog update doc test")
+                .endObject();
+
+        updateRequest.doc(doc);
+
+        UpdateResponse response = client.update(updateRequest).get();
+
+        System.out.println(String.format("index: %s update doc id: %s", response.getIndex(), response.getId()));
+
+    }
+
+    static void getDoc() {
+        GetResponse response = client.prepareGet("secisland", "secilog", "1").get();
+        String source = response.getSource().toString();
+        long version = response.getVersion();
+        String indexName = response.getIndex();
+        String type = response.getType();
+        String id = response.getId();
+
+        System.out.println(String.format("source %s version %d indexName %s type %s id %s"
+                , source, version, indexName, type, id));
+    }
+
+    static void deleteDoc() {
+        DeleteResponse deleteResponse = client.prepareDelete("secisland", "secilog", "1").get();
+        System.out.println(String.format("deleted ? %s", Boolean.toString(deleteResponse.isFound())));
+
+    }
+
 
     private static Client client;
 
